2e6be258623c5f065d8cf4be2423b4f0576dba00
Mame <119110833+ZenonX12@users.noreply.github.com>
2025-02-23 09:37:33 +0700

    Delete items.js

--- items.js
+++
@@ -1,5 +0,0 @@
-module.exports = [
-    { id: '1', name: 'Sword', price: 100, category: 'weapons', image: 'image_url' },
-    { id: '2', name: 'Shield', price: 150, category: 'weapons', image: 'image_url' },
-    // เพิ่มสินค้าอื่น ๆ ตามต้องการ
-];
